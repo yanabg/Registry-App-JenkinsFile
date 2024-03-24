a791c38a61fe10025c486dd744b7b5647cec60bb
yana <yanabgeorgieva@gmail.com>
2024-03-24 19:24:11 +0200

    fix audit issues

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const fetch = require('node-fetch');
 
 //adding a comment jsut to test the Ci in Jenkins
+//2nd comment
 suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
