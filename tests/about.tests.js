afe2e2527cea763777a56efd2322e4310e3bcd33
yana <yanabgeorgieva@gmail.com>
2024-03-24 19:12:11 +0200

    adding comment  to test the CI in Jenkins

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
+//adding a comment jsut to test the Ci in Jenkins
 suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
